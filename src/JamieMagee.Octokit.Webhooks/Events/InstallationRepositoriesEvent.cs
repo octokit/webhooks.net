2e05e2e7fd575f271a1b74941e760df6f80621d6
Jamie Magee <jamie.magee@gmail.com>
2021-09-14 09:31:34 -0700

    InstallationEvent and InstallationRepositoriesEvent

--- src/JamieMagee.Octokit.Webhooks/Events/InstallationRepositoriesEvent.cs
+++ src/JamieMagee.Octokit.Webhooks/Events/InstallationRepositoriesEvent.cs
@@ -1,11 +1,27 @@
 namespace JamieMagee.Octokit.Webhooks.Events
 {
+    using System.Collections.Generic;
     using System.Text.Json.Serialization;
     using JamieMagee.Octokit.Webhooks.Converter;
+    using JamieMagee.Octokit.Webhooks.Models;
 
     [WebhookEventType(WebhookEventType.InstallationRepositories)]
     [JsonConverter(typeof(WebhookConverter<InstallationRepositoriesEvent>))]
     public abstract record InstallationRepositoriesEvent : WebhookEvent
     {
+        [JsonPropertyName("installation")]
+        public new Models.Installation Installation { get; init; } = null!;
+
+        [JsonPropertyName("repository_selection")]
+        public InstallationRepositorySelection RepositorySelection { get; init; }
+
+        [JsonPropertyName("repositories_added")]
+        public IEnumerable<Models.InstallationRepositoriesEvent.Repository> RepositoriesAdded { get; init; }
+
+        [JsonPropertyName("repositories_removed")]
+        public IEnumerable<Models.InstallationRepositoriesEvent.Repository> RepositoriesRemoved { get; init; }
+
+        [JsonPropertyName("requester")]
+        public User? Requester { get; init; }
     }
 }
---
+++ src/JamieMagee.Octokit.Webhooks/Models/InstallationRepositoriesEvent/Repository.cs
@@ -0,0 +1,22 @@
+﻿namespace JamieMagee.Octokit.Webhooks.Models.InstallationRepositoriesEvent
+{
+    using System.Text.Json.Serialization;
+
+    public sealed record Repository
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; init; }
+
+        [JsonPropertyName("node_id")]
+        public string NodeId { get; init; } = null!;
+
+        [JsonPropertyName("name")]
+        public string Name { get; init; } = null!;
+
+        [JsonPropertyName("full_name")]
+        public string FullName { get; init; } = null!;
+
+        [JsonPropertyName("private")]
+        public bool Private { get; init; }
+    }
+}
