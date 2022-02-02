751579fbf8a4ee3e12228eb701c996ea4d4004d2
Jamie Magee <jamie.magee@gmail.com>
2022-02-02 08:02:11 -0800

    test: add unit tests for WebhookEventProcessor (#42)

---
+++ test/Octokit.Webhooks.TestUtils/ClassUtils.cs
@@ -0,0 +1,15 @@
+namespace Octokit.Webhooks.TestUtils
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class ClassUtils
+    {
+        public static Type GetEventTypeByName(string name) => Assembly.Load("Octokit.Webhooks")
+            .GetTypes()
+            .Single(t =>
+                t.FullName!.StartsWith("Octokit.Webhooks.Events.", StringComparison.InvariantCulture) &&
+                t.Name == $"{name}Event");
+    }
+}
