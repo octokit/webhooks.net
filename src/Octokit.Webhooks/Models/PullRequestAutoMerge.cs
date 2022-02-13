2a2fa2bc669f8a42bc218b1167c76ef594dd605d
Jamie Magee <jamie.magee@gmail.com>
2022-02-13 10:16:50 -0800

    fix: correctly type auto_merge property (#46)

---
+++ src/Octokit.Webhooks/Models/PullRequestAutoMerge.cs
@@ -0,0 +1,19 @@
+﻿namespace Octokit.Webhooks.Models
+{
+    using System.Text.Json.Serialization;
+
+    public sealed record PullRequestAutoMerge
+    {
+        [JsonPropertyName("enabled_by")]
+        public User EnabledBy { get; init; } = null!;
+
+        [JsonPropertyName("merge_method")]
+        public string MergeMethod { get; init; } = null!;
+
+        [JsonPropertyName("commit_title")]
+        public string CommitTitle { get; init; } = null!;
+
+        [JsonPropertyName("commit_message")]
+        public string CommitMessage { get; init; } = null!;
+    }
+}
