4f64d9022b9581c5fecb07004118cdb181966722
Jamie Magee <jamie.magee@gmail.com>
2022-07-06 21:36:01 -0700

    feat: new `referenced_workflow` property on `workflow_run`

--- src/Octokit.Webhooks/Models/DeploymentEvent/DeploymentWorkflowRun.cs
+++ src/Octokit.Webhooks/Models/DeploymentEvent/DeploymentWorkflowRun.cs
@@ -78,4 +78,7 @@ public sealed record DeploymentWorkflowRun
     [JsonPropertyName("run_started_at")]
     [JsonConverter(typeof(DateTimeOffsetConverter))]
     public DateTimeOffset RunStartedAt { get; init; }
+
+    [JsonPropertyName("referenced_workflows")]
+    public IEnumerable<ReferencedWorkflow> ReferencedWorkflows { get; init; } = null!;
 }
