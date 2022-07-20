3c9a900b94227e2327af089caa2f58fcb4940d82
martincostello <martin@martincostello.com>
2022-07-20 08:59:01 +0100

    bug: Add Unknown to ReviewState
    - Add an `Unknown` value to the `ReviewState` enum as it wasn't accounted for in #90 before it was merged.
    - Add unit tests to catch future missing `Unknown` values.

--- test/Octokit.Webhooks.Test/Converter/JsonStringEnumMemberConverterWithFallbackTests.cs
+++ test/Octokit.Webhooks.Test/Converter/JsonStringEnumMemberConverterWithFallbackTests.cs
@@ -1,5 +1,8 @@
 namespace Octokit.Webhooks.Test.Converter;
 
+using System;
+using System.Globalization;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using FluentAssertions;
@@ -25,5 +28,38 @@ public class JsonStringEnumMemberConverterWithFallbackTests
     {
         var actual = JsonSerializer.Deserialize<HookType>(input, this.options);
         actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void AllEnumerationsUseJsonStringEnumMemberConverterForJsonConverter()
+    {
+        var assembly = typeof(WebhookEvent).Assembly;
+        var types = assembly.GetTypes().ThatDeriveFrom<Enum>();
+
+        foreach (var type in types)
+        {
+            type.Should().BeDecoratedWith<JsonConverterAttribute>($"the {type.Name} enumeration must be decorated with [JsonConverter(...)]");
+
+            var converter = type.GetCustomAttribute<JsonConverterAttribute>();
+            converter.Should().NotBeNull();
+            converter!.ConverterType.Should().Be(typeof(JsonStringEnumMemberConverterWithFallback));
+        }
+    }
+
+    [Fact]
+    public void AllEnumerationsHaveUnknownMemberWithValueOfMinusOne()
+    {
+        var assembly = typeof(WebhookEvent).Assembly;
+        var types = assembly.GetTypes().ThatDeriveFrom<Enum>();
+
+        foreach (var type in types)
+        {
+            var names = Enum.GetNames(type);
+            names.Should().Contain("Unknown", $"the {type.Name} enumeration must contain an Unknown value");
+
+            var enumValue = Enum.Parse(type, "Unknown");
+            var intValue = Convert.ToInt32(enumValue, CultureInfo.InvariantCulture);
+            intValue.Should().Be(-1, $"the Unknown value of the {type.Name} enumeration must have a value of -1");
+        }
     }
 }
