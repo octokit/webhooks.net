016f41bd374fbd795f88aa6d6847833071bb19d5
Jamie Magee <jamie.magee@gmail.com>
2022-08-08 22:06:27 -0700

    fix: mark mergegroupevent as abstract (#105)

--- src/Octokit.Webhooks/Events/MergeGroupEvent.cs
+++ src/Octokit.Webhooks/Events/MergeGroupEvent.cs
@@ -7,7 +7,7 @@ using Octokit.Webhooks.Converter;
 [PublicAPI]
 [WebhookEventType(WebhookEventType.MergeGroup)]
 [JsonConverter(typeof(WebhookConverter<MergeGroupEvent>))]
-public record MergeGroupEvent : WebhookEvent
+public abstract record MergeGroupEvent : WebhookEvent
 {
     [JsonPropertyName("merge_group")]
     public Models.MergeGroupEvent.MergeGroup MergeGroup { get; init; } = null!;
