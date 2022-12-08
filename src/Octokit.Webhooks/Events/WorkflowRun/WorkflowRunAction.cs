974dc4cf29bcbaa85506972fe5a791028687aa80
Adam Warner <github@adamwarner.co.uk>
2022-12-08 16:30:07 +0000

    Add handler for workflow_run.in_progress payloads (#176)
    Co-authored-by: Jamie Magee <jamiemagee@github.com>

--- src/Octokit.Webhooks/Events/WorkflowRun/WorkflowRunAction.cs
+++ src/Octokit.Webhooks/Events/WorkflowRun/WorkflowRunAction.cs
@@ -5,6 +5,8 @@ public sealed record WorkflowRunAction : WebhookEventAction
 {
     public static readonly WorkflowRunAction Completed = new(WorkflowRunActionValue.Completed);
 
+    public static readonly WorkflowRunAction InProgress = new(WorkflowRunActionValue.InProgress);
+
     public static readonly WorkflowRunAction Requested = new(WorkflowRunActionValue.Requested);
 
     private WorkflowRunAction(string value)
