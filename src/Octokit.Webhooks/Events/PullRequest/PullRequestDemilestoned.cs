ddb8ec46111bb1594bd69d9942f4ba2eae04f136
Jamie Magee <JamieMagee@github.com>
2023-04-06 22:54:40 -0700

    feat: add `pull_request.{demilestoned, milestoned}` events
    Closes #223

---
+++ src/Octokit.Webhooks/Events/PullRequest/PullRequestDemilestoned.cs
@@ -0,0 +1,14 @@
+﻿namespace Octokit.Webhooks.Events.PullRequest;
+
+using Milestone = Octokit.Webhooks.Models.Milestone;
+
+[PublicAPI]
+[WebhookActionType(PullRequestActionValue.Demilestoned)]
+public sealed record PullRequestDemilestoned : PullRequestEvent
+{
+    [JsonPropertyName("action")]
+    public override string Action => PullRequestAction.Demilestoned;
+
+    [JsonPropertyName("milestone")]
+    public Milestone Milestone { get; init; } = null!;
+}
