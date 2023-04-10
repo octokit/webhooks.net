27b767ca84c8d7fc5d9b761a730259cf83b12bbf
Jamie Magee <jamie.magee@gmail.com>
2023-04-10 16:33:35 -0700

    feat: add `installation_target` event (#236)

---
+++ src/Octokit.Webhooks/Models/InstallationTargetEvent/ChangesLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace Octokit.Webhooks.Models.InstallationTargetEvent;
+
+[PublicAPI]
+public record ChangesLogin
+{
+    [JsonPropertyName("from")]
+    public string From { get; init; } = null!;
+}
