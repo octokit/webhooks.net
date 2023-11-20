75e45b3583fc31b33859e5eff124d834a9ff5b17
Jamie Magee <jamie.magee@gmail.com>
2023-11-20 13:55:35 -0800

    chore(deps): upgrade to .net 8 sdk (#393)

--- samples/AzureFunctions/MyWebhookEventProcessor.cs
+++ samples/AzureFunctions/MyWebhookEventProcessor.cs
@@ -6,25 +6,18 @@ using Octokit.Webhooks;
 using Octokit.Webhooks.Events;
 using Octokit.Webhooks.Events.PullRequest;
 
-public class MyWebhookEventProcessor : WebhookEventProcessor
+public class MyWebhookEventProcessor(ILogger<MyWebhookEventProcessor> logger) : WebhookEventProcessor
 {
-    private readonly ILogger<MyWebhookEventProcessor> logger;
-
-    public MyWebhookEventProcessor(ILogger<MyWebhookEventProcessor> logger)
-    {
-        this.logger = logger;
-    }
-
     protected override async Task ProcessPullRequestWebhookAsync(WebhookHeaders headers, PullRequestEvent pullRequestEvent, PullRequestAction action)
     {
         switch (action)
         {
             case PullRequestActionValue.Opened:
-                this.logger.LogInformation("pull request opened");
+                logger.LogInformation("pull request opened");
                 await Task.Delay(1000);
                 break;
             default:
-                this.logger.LogInformation("Some other pull request event");
+                logger.LogInformation("Some other pull request event");
                 await Task.Delay(1000);
                 break;
         }
