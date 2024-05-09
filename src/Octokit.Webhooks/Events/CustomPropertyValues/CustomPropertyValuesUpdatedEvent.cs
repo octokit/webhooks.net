4d333660d47e772221f49562783f23de33399cb2
Colby Williams <ColbyLWilliams@gmail.com>
2024-05-09 01:10:10 -0500

    correct objects for custom_property_values events

--- src/Octokit.Webhooks/Events/CustomPropertyValues/CustomPropertyValuesUpdatedEvent.cs
+++ src/Octokit.Webhooks/Events/CustomPropertyValues/CustomPropertyValuesUpdatedEvent.cs
@@ -6,4 +6,10 @@ public sealed record CustomPropertyValuesUpdatedEvent : CustomPropertyValuesEven
 {
     [JsonPropertyName("action")]
     public override string Action => CustomPropertyValuesAction.Updated;
+
+    [JsonPropertyName("new_property_values")]
+    public IEnumerable<Models.CustomPropertyValuesEvent.CustomPropertyValue> NewPropertyValues { get; init; } = null!;
+
+    [JsonPropertyName("old_property_values")]
+    public IEnumerable<Models.CustomPropertyValuesEvent.CustomPropertyValue> OldPropertyValues { get; init; } = null!;
 }
