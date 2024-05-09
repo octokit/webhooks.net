b0742de3aa84fec24f1522bbddc711048a7aada4
Colby Williams <ColbyLWilliams@gmail.com>
2024-05-09 01:07:20 -0500

    fix bug in (released) custom_property_values event

--- src/Octokit.Webhooks/Events/CustomPropertyValues/CustomPropertyValuesUpdatedEvent.cs
+++ src/Octokit.Webhooks/Events/CustomPropertyValues/CustomPropertyValuesUpdatedEvent.cs
@@ -2,7 +2,7 @@ namespace Octokit.Webhooks.Events.CustomPropertyValues;
 
 [PublicAPI]
 [WebhookActionType(CustomPropertyValuesActionValue.Updated)]
-public abstract record CustomPropertyValuesUpdatedEvent : CustomPropertyValuesEvent
+public sealed record CustomPropertyValuesUpdatedEvent : CustomPropertyValuesEvent
 {
     [JsonPropertyName("action")]
     public override string Action => CustomPropertyValuesAction.Updated;
