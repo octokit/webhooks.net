4beaac7bc567ec85319cd11e84c1f417a71aec39
Jamie Magee <jamie.magee@gmail.com>
2024-08-19 13:54:32 -0700

    fix: move common properties from `DeploymentProtectionRuleRequestedEvent` to `DeploymentProtectionRuleRequestedEvent` (#549)
    Co-authored-by: Nick Floyd <139819+nickfloyd@users.noreply.github.com>

--- src/Octokit.Webhooks/Events/DeploymentProtectionRuleEvent.cs
+++ src/Octokit.Webhooks/Events/DeploymentProtectionRuleEvent.cs
@@ -3,4 +3,20 @@ namespace Octokit.Webhooks.Events;
 [PublicAPI]
 [WebhookEventType(WebhookEventType.DeploymentProtectionRule)]
 [JsonConverter(typeof(WebhookConverter<DeploymentProtectionRuleEvent>))]
-public abstract record DeploymentProtectionRuleEvent : WebhookEvent;
+public abstract record DeploymentProtectionRuleEvent : WebhookEvent
+{
+    [JsonPropertyName("environment")]
+    public string Environment { get; init; } = null!;
+
+    [JsonPropertyName("event")]
+    public string Event { get; init; } = null!;
+
+    [JsonPropertyName("deployment_callback_url")]
+    public string DeploymentCallbackUrl { get; init; } = null!;
+
+    [JsonPropertyName("deployment")]
+    public Models.DeploymentEvent.Deployment Deployment { get; init; } = null!;
+
+    [JsonPropertyName("pull_requests")]
+    public IEnumerable<Models.PullRequestEvent.PullRequest> PullRequests { get; init; } = null!;
+}
