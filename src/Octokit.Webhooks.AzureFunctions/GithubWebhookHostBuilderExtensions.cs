b8c2e36a6bcdbc2896ce532b87daf1552a75915f
Anton Ganhammar <ganhammar@gmail.com>
2024-09-06 08:58:30 +0200

    feat: add additional configuration option

--- src/Octokit.Webhooks.AzureFunctions/GithubWebhookHostBuilderExtensions.cs
+++ src/Octokit.Webhooks.AzureFunctions/GithubWebhookHostBuilderExtensions.cs
@@ -1,5 +1,7 @@
-﻿namespace Octokit.Webhooks.AzureFunctions;
+namespace Octokit.Webhooks.AzureFunctions;
 
+using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,4 +16,18 @@ public static class GithubWebhookHostBuilderExtensions
     public static IHostBuilder ConfigureGitHubWebhooks(this IHostBuilder hostBuilder, string? secret = null) =>
         hostBuilder.ConfigureServices(services =>
             services.AddOptions<GitHubWebhooksOptions>().Configure(options => options.Secret = secret));
+
+    /// <summary>
+    /// Configures GitHub webhook function.
+    /// </summary>
+    /// <param name="hostBuilder"><see cref="IHostBuilder" /> instance.</param>
+    /// <param name="configure">A function that takes an <see cref="IConfiguration" /> instance and expects the secret you have configured in GitHub in return.</param>
+    /// <returns>Returns <see cref="IHostBuilder" /> instance.</returns>
+    public static IHostBuilder ConfigureGitHubWebhooks(this IHostBuilder hostBuilder, Func<IConfiguration, string> configure)
+    {
+        ArgumentNullException.ThrowIfNull(configure);
+
+        return hostBuilder.ConfigureServices((context, services) =>
+            services.AddOptions<GitHubWebhooksOptions>().Configure(options => options.Secret = configure(context.Configuration)));
+    }
 }
