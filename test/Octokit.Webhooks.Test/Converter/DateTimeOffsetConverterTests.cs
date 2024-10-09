62c861dd6f9744c3fd0b68dbdaf680164d92a65d
Martin Costello <martin@martincostello.com>
2024-10-09 14:58:15 +0100

    Use long for numeric dates (#591)
    * Use long for numeric dates
    
    Use `long` instead of `int` to avoid an implicit conversion and the Y2038 bug.
    
    * Add Y2038 tests
    
    Add unit tests for handling 64-bit integer dates.

--- test/Octokit.Webhooks.Test/Converter/DateTimeOffsetConverterTests.cs
+++ test/Octokit.Webhooks.Test/Converter/DateTimeOffsetConverterTests.cs
@@ -17,6 +17,7 @@ public class DateTimeOffsetConverterTests
     };
 
     private readonly DateTimeOffset expected = new(2019, 05, 15, 15, 20, 40, default);
+    private readonly DateTimeOffset expectedY2038 = DateTimeOffset.FromUnixTimeSeconds(int.MaxValue + 1L);
 
     [Theory]
     [InlineData(@"""2019-05-15T15:20:40Z""")]
@@ -27,17 +28,33 @@ public class DateTimeOffsetConverterTests
         result.Should().Be(this.expected);
     }
 
+    [Theory]
+    [InlineData(@"""2038-01-19T03:14:08Z""")]
+    [InlineData(@"""2038-01-19T03:14:08.000+00:00""")]
+    public void CanDeserializeStringY2038(string input)
+    {
+        var result = JsonSerializer.Deserialize<DateTimeOffset>(input, this.options);
+        result.Should().Be(this.expectedY2038);
+    }
+
     [Fact]
     public void CanDeserializeNumber()
     {
-        var result = JsonSerializer.Deserialize<DateTimeOffset>(@"1557933640", this.options);
+        var result = JsonSerializer.Deserialize<DateTimeOffset>("1557933640", this.options);
         result.Should().Be(this.expected);
+    }
+
+    [Fact]
+    public void CanDeserializeNumberY2038()
+    {
+        var result = JsonSerializer.Deserialize<DateTimeOffset>("2147483648", this.options);
+        result.Should().Be(this.expectedY2038);
     }
 
     [Fact]
     public void DeserializeThrowsForNull()
     {
-        var result = () => JsonSerializer.Deserialize<DateTimeOffset>(@"null", this.options);
+        var result = () => JsonSerializer.Deserialize<DateTimeOffset>("null", this.options);
         result.Should().Throw<JsonException>();
     }
 
