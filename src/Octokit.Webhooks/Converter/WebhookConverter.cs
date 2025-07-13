9251df1a32e64834fadb0faff9343ed945eefd2d
martincostello <martin@martincostello.com>
2025-07-13 09:40:57 +0100

    Support custom repository_dispatch actions
    - Add support for deserializing user-defined `repository_dispatch` payloads.
    - Simplify reflection to get attributes.
    - Use dictionary instead of iterating it.
    - Use constant instead of computing string per-deserialization.

--- src/Octokit.Webhooks/Converter/WebhookConverter.cs
+++ src/Octokit.Webhooks/Converter/WebhookConverter.cs
@@ -1,21 +1,22 @@
 ﻿namespace Octokit.Webhooks.Converter;
 
 using System.Linq;
+using System.Reflection;
 
 [PublicAPI]
 public class WebhookConverter<T> : JsonConverter<T>
     where T : WebhookEvent
 {
-    private readonly IDictionary<string, Type> types;
+    private readonly Dictionary<string, Type> types;
 
     public WebhookConverter()
     {
         var type = typeof(T);
         this.types = this.GetType().Assembly.GetTypes()
             .Where(x => type.IsAssignableFrom(x) && x is { IsClass: true, IsAbstract: false } &&
-                        Attribute.GetCustomAttribute(x, typeof(WebhookActionTypeAttribute)) is not null)
+                        x.GetCustomAttribute<WebhookActionTypeAttribute>() is not null)
             .ToDictionary(
-                y => ((WebhookActionTypeAttribute)Attribute.GetCustomAttribute(y, typeof(WebhookActionTypeAttribute))!).ActionType,
+                y => y.GetCustomAttribute<WebhookActionTypeAttribute>()!.ActionType,
                 y => y);
     }
 
@@ -27,12 +28,30 @@ public class WebhookConverter<T> : JsonConverter<T>
         }
 
         using var jsonDocument = JsonDocument.ParseValue(ref reader);
-        if (!jsonDocument.RootElement.TryGetProperty(nameof(WebhookEvent.Action).ToLower(CultureInfo.InvariantCulture), out var action))
+        if (!jsonDocument.RootElement.TryGetProperty("action", out var action))
         {
             throw new JsonException();
         }
 
-        var type = this.types.FirstOrDefault(x => x.Key == action.GetString()).Value ?? throw new JsonException();
+        Type? type = null;
+
+        var actionValue = action.GetString();
+
+        if (actionValue is not null && this.types.TryGetValue(actionValue, out var payloadType))
+        {
+            type = payloadType;
+        }
+
+        // repository_dispatch events can have custom user-defined actions values
+        if (type == null && typeToConvert == typeof(Events.RepositoryDispatchEvent))
+        {
+            type = typeof(Events.RepositoryDispatch.RepositoryDispatchCustomEvent);
+        }
+
+        if (type is null)
+        {
+            throw new JsonException();
+        }
 
         var jsonObject = jsonDocument.RootElement.GetRawText();
         return (T)JsonSerializer.Deserialize(jsonObject, type, options)!;
