0f7c848f7601a167ce00ce3c059cd08982d9e907
Jamie Magee <jamie.magee@gmail.com>
2026-07-22 21:16:27 -0700

    Honor caller JSON options in WebhookConverter

--- src/Octokit.Webhooks/Converter/WebhookConverter.cs
+++ src/Octokit.Webhooks/Converter/WebhookConverter.cs
@@ -2,7 +2,7 @@ namespace Octokit.Webhooks.Converter;
 
 using System.Collections.Frozen;
 using System.Linq;
-using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization.Metadata;
 
 [PublicAPI]
@@ -38,8 +38,7 @@ public sealed class WebhookConverter<T> : JsonConverter<T>
             throw new JsonException();
         }
 
-        // Note: calling GetTypeInfo(type, options) can cause a stack overflow
-        var jsonTypeInfo = WebhookEventJsonSerializerContext.Default.GetTypeInfo(type) ?? throw new JsonException();
+        var jsonTypeInfo = WebhookConverter.GetTypeInfo(type, options);
 
         return (T)JsonSerializer.Deserialize(ref reader, jsonTypeInfo)!;
     }
@@ -48,8 +47,7 @@ public sealed class WebhookConverter<T> : JsonConverter<T>
 
     public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
     {
-        // Note: calling GetTypeInfo(type, options) can cause a stack overflow
-        var jsonTypeInfo = WebhookEventJsonSerializerContext.Default.GetTypeInfo(value.GetType()) ?? throw new JsonException();
+        var jsonTypeInfo = WebhookConverter.GetTypeInfo(value.GetType(), options);
         JsonSerializer.Serialize(writer, value, jsonTypeInfo);
     }
 
@@ -84,9 +82,79 @@ public sealed class WebhookConverter<T> : JsonConverter<T>
 /// </summary>
 internal static partial class WebhookConverter
 {
+    private static readonly ConditionalWeakTable<JsonSerializerOptions, JsonSerializerOptions> SerializerOptions = [];
+
+    internal static JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
+    {
+        var serializerOptions = SerializerOptions.GetValue(options, static key => CreateSerializerOptions(key));
+        var typeInfo = serializerOptions.GetTypeInfo(type);
+
+        return !IsWebhookConverter(typeInfo.Converter)
+            ? typeInfo
+            : throw new JsonException($"Unable to resolve JSON metadata for webhook event type '{type}'.");
+    }
+
     /// <summary>
     /// This method is implemented by the source generator to return a list of all types decorated with the <see cref="WebhookActionTypeAttribute"/>, along with their corresponding action type string.
     /// </summary>
     /// <returns>An enumerable of tuples containing the type and its corresponding action type string.</returns>
     internal static partial IEnumerable<(Type Type, string ActionType)> GetActionTypes();
+
+    private static JsonSerializerOptions CreateSerializerOptions(JsonSerializerOptions options)
+    {
+        var serializerOptions = new JsonSerializerOptions(options);
+
+        for (var index = serializerOptions.Converters.Count - 1; index >= 0; index--)
+        {
+            if (IsWebhookConverter(serializerOptions.Converters[index]))
+            {
+                serializerOptions.Converters.RemoveAt(index);
+            }
+        }
+
+        var typeInfoResolvers = serializerOptions.TypeInfoResolverChain.ToArray();
+        if (typeInfoResolvers.Length == 0 && serializerOptions.TypeInfoResolver is { } typeInfoResolver)
+        {
+            typeInfoResolvers = [typeInfoResolver];
+        }
+
+        serializerOptions.TypeInfoResolverChain.Clear();
+
+        IJsonTypeInfoResolver generatedResolver = WebhookEventJsonSerializerContext.Default;
+        foreach (var modifier in typeInfoResolvers
+            .OfType<DefaultJsonTypeInfoResolver>()
+            .SelectMany(static resolver => resolver.Modifiers))
+        {
+            generatedResolver = generatedResolver.WithAddedModifier(modifier);
+        }
+
+        foreach (var resolver in typeInfoResolvers.Where(static resolver => resolver is not DefaultJsonTypeInfoResolver))
+        {
+            serializerOptions.TypeInfoResolverChain.Add(new NonRecursiveTypeInfoResolver(resolver));
+        }
+
+        serializerOptions.TypeInfoResolverChain.Add(generatedResolver);
+        serializerOptions.MakeReadOnly();
+
+        return serializerOptions;
+    }
+
+    private static bool IsWebhookConverter(JsonConverter converter)
+    {
+        var converterType = converter.GetType();
+        return converterType.IsGenericType &&
+               converterType.GetGenericTypeDefinition() == typeof(WebhookConverter<>);
+    }
+
+    private sealed class NonRecursiveTypeInfoResolver(IJsonTypeInfoResolver resolver) : IJsonTypeInfoResolver
+    {
+        public JsonTypeInfo? GetTypeInfo(Type type, JsonSerializerOptions options)
+        {
+            var typeInfo = resolver.GetTypeInfo(type, options);
+
+            return typeInfo is not null && IsWebhookConverter(typeInfo.Converter)
+                ? null
+                : typeInfo;
+        }
+    }
 }
